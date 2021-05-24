f870e6537e50f83a2561fd3c2f9770b2248ac511
yyuvimali <82664190+yyuvimali@users.noreply.github.com>
2021-05-24 23:12:30 +0530

    error page 404

---
+++ app.js
@@ -0,0 +1,19 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+
+const app=express();
+
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(shopRoutes);
+app.use(adminRoutes);
+
+app.use(function(req,res,next){
+    res.status(404).send('<h1>Page not found</h1>')
+});
+
+app.listen(8000);
+
